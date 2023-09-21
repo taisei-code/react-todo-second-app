16acea7fda4ff8fd3b1a704b0dfbd8743c05ce0f
Taisei <taisei96.web@gmail.com>
2023-09-21 13:48:03 +0900

    実装

--- src/App.js
+++ src/App.js
@@ -13,15 +13,45 @@ function App() {
 
   // 更新内容input入力時のstate更新
   //リストから更新対象のオブジェクトのtargetプロパティのみを更新する
-  const inputUpdate = () => {
+  const inputUpdate = (event, i) => {
     setList(
       // mapで更新対象オブジェクトのみを変更した新規配列を作成し、stateにセットする
       list.map((val, index) =>
+        // index が一致していればtargetを上書き
         index === i ? { ...val, target: event.target.value } : { ...val }
       )
     );
-  }
+  };
 
+  // todolistへの追加 （inputで入力した値を,オブジェクトにしてsetStateで追加）
+  const add = () => {
+    // 値がない場合は追加しない
+    if (!registInput.current.value) return;
+    setList((prevList) => [
+      // 直前のstateをスプレッド構文で展開
+      ...prevList,
+      // 新規オブジェクトを追加
+      { title: registInput.current.value, target: "" },
+    ]);
+  };
+
+  // todolistの更新
+  const upd = (i) => {
+    setList(
+      // 更新内容inputを変更した時点でstateの配列に格納されているオブジェクトのtargetは
+      // 更新されているため、更新ボタン押下時は更新対象オブジェクトのterget→titleで更新した
+      // 新規配列をmapで作成し、stateにセットする
+      list.map((val, index) =>
+        index === i ? { ...val, title: val.target } : { ...val }
+      )
+    );
+  };
+
+  // todolistの削除
+  const del = (i) => {
+    // indexが一致したtodoは除去する
+    setList(list.filter((_, index) => index !== i));
+  };
 
   return (
     <div className="App">
@@ -29,25 +59,31 @@ function App() {
 
       <div>
         <input type="text" ref={registInput}></input>
-        <button className="button">登録</button>
+        <button className="button" onClick={add}>
+          登録
+        </button>
         <button className="button">クリア</button>
       </div>
 
-      <h1 style={ { textAlign: "left" } }>ToDo List</h1>
+      <h1 style={{ textAlign: "left" }}>ToDo List</h1>
       <ul style={{ textAlign: "left" }}>
         {list.map((todoList, i) => (
           <li key={i}>
-            <button>削除</button>
+            {todoList.title}
             <input
               type="text"
               value={todoList.target}
-              onChange={(e) => inputUpdate(e, i) }
+              onChange={(e) => inputUpdate(e, i)}
             ></input>
-            <button>更新</button>
+            <button className="button" onClick={() => upd(i)}>
+              更新
+            </button>
+            <button className="button" onClick={() => del(i)}>
+              削除
+            </button>
           </li>
         ))}
       </ul>
-
     </div>
   );
 }
